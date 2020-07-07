2c75f077b3ecd1482cec3caec985e104436c5e91
Miroslav <miroslav.kartashov@gmail.com>
2020-07-07 15:21:15 +0300

    исправил косяк  , с  назваанием методов  ((  (затупил  хотел красиво )) ) Исправил   подсказку самопроверку которую  не пофиксил сразу

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,10 +9,10 @@ public class Main {
 // 1. Написать программу, которая загадывает случайное число от 0 до 9, и пользователю дается 3 попытки угадать это число.
 // При каждой попытке компьютер должен сообщить больше ли указанное пользователем число чем загаданное, или меньше.
 // После победы или проигрыша выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
-        GuessTheNumber();
-        GuessTheWord();
+        guessTheNumber();
+        guessTheWord();
     }
-    public static void GuessTheNumber() {
+    public static void guessTheNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("У Вас всего 3 попытки угадать числo." );
         int range = 9;
@@ -26,7 +26,7 @@ public class Main {
                 System.out.println("Вы выиграли!");
                 System.out.println("Повторить игру еще раз? 1 – да / 0 – нет!");
                 replay = scanner.nextInt();
-                if (replay == 1) GuessTheNumber();
+                if (replay == 1) guessTheNumber();
                 else System.out.println("Cпасибо, игра закончена");
                 break;
             } else if (inputNumber > number)  {
@@ -38,7 +38,7 @@ public class Main {
                 System.out.println("УППсс  " + effort + " попытки использовано " );
                 System.out.println("Повторить игру еще раз? 1 – да / 0 – нет!");
                 replay = scanner.nextInt();
-                if (replay == 1) GuessTheNumber();
+                if (replay == 1) guessTheNumber();
                 else System.out.println("Cпасибо, игра закончена");
                 break;
             }
@@ -53,7 +53,7 @@ public class Main {
 //сравнивает его с загаданным словом и сообщает правильно ли ответил пользователь.
 // Если слово не угадано, компьютер показывает буквы которые стоят на своих местах.
 
-    public static void GuessTheWord() {
+    public static void guessTheWord() {
         Scanner scanner = new Scanner(System.in);
         Random randomValue = new Random();
         String[] words = {"apple", "orange", "lemon", "banana", "apricot",
@@ -61,8 +61,8 @@ public class Main {
                 "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
                 "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
         int n = randomValue.nextInt(words.length);
-        System.out.println(words[n]);
-        System.out.println("Копьютер загадал  слово из списка:" );
+//        System.out.println(words[n]);    самопроверка
+       System.out.println("Копьютер загадал  слово из списка:" );
         System.out.println(Arrays.toString(words));
         System.out.println("Ваша задача отгадать это слово. ");
         String word;
